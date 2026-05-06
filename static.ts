13db97ed71eb69a5529b36518e333e60e8245221
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-05-06 16:29:57 +0600

    Add Counter class with static count property and increment/decrement methods

---
+++ static.ts
@@ -0,0 +1,44 @@
+// static ->> a function that belongs to the class itself rather than to any specific instance of that class
+
+class Counter {
+  // by using static this will have only one reference
+  static count: number = 0;
+  increment() {
+    // Property 'count' does not exist on type 'Counter'. Did you mean to access the static member 'Counter.count' instead?
+    return (Counter.count = Counter.count + 1);
+  }
+  decrement() {
+    return (Counter.count = Counter.count - 1);
+  }
+}
+
+// before using static
+
+// //! this instance1 is working on a memory
+// const instance1 = new Counter();
+// console.log("instance1", instance1.increment()); ->1
+// console.log("instance1", instance1.increment()); ->2
+// console.log("instance1", instance1.increment()); ->3
+// console.log("instance1", instance1.increment()); ->4
+
+// //! this instance2 is working on a different memory
+// const instance2 = new Counter();
+// console.log("instance2", instance2.increment()); -> 1
+// console.log("instance2", instance2.increment()); -> 2
+// console.log("instance2", instance2.increment()); -> 3
+// console.log("instance2", instance2.increment()); -> 4
+
+// after using static
+
+// ! both instance1 & instance2 are using same memory
+const instance1 = new Counter();
+console.log("instance1", instance1.increment()); //->1
+console.log("instance1", instance1.increment()); //->2
+console.log("instance1", instance1.increment()); //->3
+console.log("instance1", instance1.increment()); //->4
+
+const instance2 = new Counter();
+console.log("instance2", instance2.increment()); //-> 5
+console.log("instance2", instance2.increment()); //-> 6
+console.log("instance2", instance2.increment()); //-> 7
+console.log("instance2", instance2.increment()); //-> 8
